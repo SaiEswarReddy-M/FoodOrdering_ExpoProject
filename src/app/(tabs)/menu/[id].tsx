1da67c5bee8dca9ea18af0e7b53e8f8b8f6ead20
Sai Eswar Reddy Menakuru <menakurusaieswarreddy@gmail.com>
2024-09-26 18:54:15 +0530

    Added Select Size UI in ProductDetail Screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,13 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
+import products from "@assets/data/products";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+
+const sizes = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
+  const product = products.find((p) => p.id.toString() == id);
+  if (!product) {
+    return <Text>Product Not Found</Text>;
+  } //extra lins to avoid error
+
   return (
     <View>
-      <Stack.Screen options={{ title: "Details:" + id }} />
-      <Text style={styles.textStyles}>ProductDetailsScreen for id:{id}</Text>
+      <Stack.Screen options={{ title: product?.name || "Default Title" }} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
+      <Text>Select Size</Text>
+      <View style={styles.sizes}>
+        {sizes.map((size) => (
+          <View style={styles.size}>
+            <Text style={styles.sizeText} key={size}>
+              {size}
+            </Text>
+          </View>
+        ))}
+      </View>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 };
@@ -15,7 +38,36 @@ const ProductDetailsScreen = () => {
 export default ProductDetailsScreen;
 
 const styles = StyleSheet.create({
-  textStyles: {
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    // height: "50%",
+    aspectRatio: 1,
+  },
+  price: {
     fontSize: 20,
+    fontWeight: "bold",
+    color: "green",
+  },
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: "gainsboro",
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
